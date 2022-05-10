a9a49aa05ee46c70bf70b56d8fff71502c2c259f
Senjuuuuu <senjuu.qkp@gmail.com>
2022-05-10 20:50:54 +0700

    done

--- src/app.js
+++ src/app.js
@@ -14,8 +14,19 @@ viewEngine(app)
 //routes
 initWebRoute(app)
 
-// let port = process.env.PORT || 8080;
-// app.listen(port, () => {
-//     console.log('Chat bot is running from port: ' + port);
-// });
+// Handle error
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+  });
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+  
 module.exports = app
